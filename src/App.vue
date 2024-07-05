d39e44f86b9ab916dc1126db2c81e002adebbeb6
outfoxedmeng <outfoxed@163.com>
2024-07-05 13:58:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/home' }">homepage</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      <a href="/">promotion management</a>
+    </el-breadcrumb-item>
+    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+  </el-breadcrumb>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style></style>
